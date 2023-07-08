2adc73d09cdb7de46d829b8e4ad7573651a7c8ff
skullboi990 <132878139+skullboi990@users.noreply.github.com>
2023-07-08 22:24:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,50 @@ const onProgress = (event) => {
     progressBar.classList.remove('hide');
   }
 };
-document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+const modelViewer = document.querySelector('model-viewer');
+modelViewer.addEventListener('progress', onProgress);
+
+// Start the rotation
+let direction = 0.1; // change this value to adjust rotation speed
+let angle = 0;
+let rotating = true;
+
+function rotate() {
+  if (!rotating) {
+    requestAnimationFrame(rotate);
+    return;
+  }
+  
+  angle += direction;
+  if (angle > 50) {
+    direction = -0.1;
+  } else if (angle < -50) {
+    direction = 0.1;
+  }
+  modelViewer.setAttribute('camera-orbit', `${angle}deg 75deg auto`);
+  requestAnimationFrame(rotate);
+}
+
+document.addEventListener('pointerdown', () => {
+  rotating = false;
+});
+
+document.addEventListener('pointerup', () => {
+  rotating = true;
+});
+
+window.addEventListener('load', (event) => {
+  const modelViewer = document.querySelector('model-viewer');
+
+  // Dispatch synthetic click event
+  const click = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: true
+  });
+  modelViewer.dispatchEvent(click);
+
+  // Start the rotation
+  rotate();
+});
